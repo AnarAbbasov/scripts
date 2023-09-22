a1cdc58df2dbb27a32a36caa99f6446b52dc53e1
Anar Abbasov <anar_abbasov@hotmail.com>
2023-09-22 09:17:06 -0700

    updated class

--- my_class.py
+++ my_class.py
@@ -5,12 +5,12 @@ class MyClass:
     my_tuple=()
 
     def __init__(self) -> None:
-        my_list=[x for x in range(200)]
-        my_dic={d for d in range(60)}
-        my_set=(s for s in range(400))
-        my_tuple=(1,2,3,4,5)
+        self.my_list=[x for x in range(200)]
+        self.my_dic={d for d in range(60)}
+        self.my_set=(s for s in range(400))
+        self.my_tuple=(1,2,3,4,5)
 
 
-    def show_list():
-        print(my_list)
-        
+    def show_list(self):
+        print(self.my_list)
+
--- test_decorators.py
+++ test_decorators.py
@@ -1,10 +1,11 @@
 from sole_decorators import get_log
-
+import my_class
 
 @get_log
 def myprinting(text):
     print (text)
     
+anar_class= my_class.MyClass()
 
-
+anar_class.show_list()
 myprinting('clen')
